c5d6c610e6231d1144efd85c7afb06dee424ff3f
zed <zed@gem-pselabui-iMac.local>
2017-02-15 11:57:27 +0900

    b1 updated

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import *
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.utils import timezone
+
 # Create your views here.
 def test(request): #login and test
 	return render(request, 'tpage.html', {})
@@ -10,15 +12,23 @@ def login(request): #login and test
 	return render(request, 'login.html', {})
 
 def cReq(request):
-	items = T_PROD_TYPE.objects.all()
-	template = 'b1.html'
-	return render(request, template, {'items': items})
+    items = T_PROD_TYPE.objects.all()
+    
+    #comparing phase
+    if T_ORDER_INFO.objects.all().count() != 0:
+        last_order = T_ORDER_INFO.objects.latest('ITEM_ID') #earliest
+    else:
+        last_order = 0
+    
+    template = 'b1.html'
+    return render(request, template, {'items': items, 'last_order': last_order, 'new_order': last_order+1})
 
 def cReq_newRequest(request):
-	item = request.POST['item']
-	num = request.POST['num']
-	print(item, num)
-	return HttpResponseRedirect(reverse('main:cList'))
+    item = request.POST['item']
+    num = request.POST['num']
+    order_date = timezone.now()
+    print(item, num, order_date) #주문수량, 주문제품번호
+    return HttpResponseRedirect(reverse('main:cList'))
 
 def cList(request):
 	return render(request, 'b2.html', {})
